9029b6ba22f9e26082aad0b8493f3be460b2aa31
RianMoraes <rian.moraes2702@gmail.com>
2025-12-16 10:51:27 -0300

    refiz o js, add remove event

--- scripts.js
+++ scripts.js
@@ -1,43 +1,35 @@
+const input = document.querySelector("input");
+const form = document.querySelector("form")
+const lista = document.getElementById("lista")
 
-//obtendo elementos do input
-const form = document.getElementsByClassName("app")
-const inputs = document.getElementById("addNewItem");
-const button = document.querySelector("form")
-
-
-inputs.addEventListener("input", () => {
-    const value = inputs.value
-    // console.log(value)
-})
-button.addEventListener("submit", (event) => {
+form.addEventListener("submit", (event) => {
     event.preventDefault()
-    const value = inputs.value
-    // console.log(value)
-})
-/* div.items é todos, div.item é um com input span e img */
-
-const items = document.querySelector(".items")
-const item = document.querySelector(".item")
-const newItem = item.cloneNode(true) // clona o item com input span e img
-const input = newItem.querySelector("input") // seleciona o input do novo item
-const span = newItem.querySelector("span") // seleciona o span do novo item
-const img = newItem.querySelector("img") // seleciona a img do novo item
-button.addEventListener("submit", (event) => {
-    event.preventDefault()
-    const value = inputs.value  
-    span.textContent = value // atribui o valor do input ao span
-    input.value = "" // limpa o input
-    items.prepend(newItem) // adiciona o novo item à lista de itens
+    lista.prepend(createItemLista())
+    input.value = ""
 })
 
+function createItemLista() {
+    const divItemLista = document.createElement("div")
+    divItemLista.classList.add("item")
 
+    const chkItemlista = document.createElement("input")
+    chkItemlista.type = "checkbox"
 
-/*
-const guests = document.querySelector("ul") //
-const newGuest = document.createElement("li") //
-newGuest.classList.add("guest") //
-const guestName = document.createElement("span") //
-guestName.textContent = "rian"
-newGuest.append(guestName)
-guests.prepend(newGuest)
-*/
+    const spanItemContent = document.createElement("span")
+    spanItemContent.textContent = input.value
+
+    const imgItemLista = document.createElement("img")
+    imgItemLista.src = "./assets/icons/lixeira.svg"
+    imgItemLista.alt = "lixeira icon"
+
+    divItemLista.append(chkItemlista, spanItemContent, imgItemLista)
+    return divItemLista
+}
+lista.addEventListener("click", (event) => {
+    const clickedElement = event.target
+
+    if (clickedElement.tagName === "IMG") {
+        const item = clickedElement.closest(".item")
+        item.remove()
+    }
+})
